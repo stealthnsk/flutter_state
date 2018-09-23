94131b3d131d9e48cc08c24b8388afe8801c356a
Andrey Gavrilov <stealthnsk@gmail.com>
2018-09-23 22:03:16 +0700

    Step 2.1

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
 
-  Random rand = Random();
-
   TabController _tabController;
 
   final List<Tab> myTabs = <Tab>[
