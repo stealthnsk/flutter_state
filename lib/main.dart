eed19fd6294617b3fa2491706c0dcee1f4b0e092
Andrey Gavrilov <stealthnsk@gmail.com>
2018-09-23 21:32:18 +0700

    Step 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Sample app'),
-        ),
-        body: new MyHomePage(),
-      ),
+      home: new MyHomePage(),
     );
   }
 }
@@ -27,14 +22,42 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
 
   Random rand = Random();
 
+  TabController _tabController;
+
+  final List<Tab> myTabs = <Tab>[
+    new Tab(text: 'FIRST'),
+    new Tab(text: 'SECOND'),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(vsync: this, length: myTabs.length);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new ListView.builder(itemBuilder: (BuildContext context, int index) {
-      return Text('Random number ${rand.nextInt(100)}',);
-    });
+    return Scaffold(
+        appBar: AppBar(
+        title: Text('Sample app'),
+    ),
+    body: new TabBarView(
+        controller: _tabController,
+        children: [
+          new ListView.builder(itemBuilder: (BuildContext context, int index) {
+            return Text('Random number ${rand.nextInt(100)}',);
+          }),
+          Text('Second tab'),
+        ],),
+      bottomNavigationBar: new TabBar(
+        controller: _tabController,
+        tabs: myTabs,
+        labelColor: Colors.blue,
+      ),
+    );
   }
 }
